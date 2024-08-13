dddf56908446765121e705a3cce5f25a2f2db308
publicvoid1337 <oliver.fritz-default@protonmail.com>
2024-08-13 17:57:20 +0200

    Added append method to renderer

--- src/renderer.rs
+++ src/renderer.rs
@@ -10,88 +10,76 @@ use ScreenPart::{Head, Body, Tail};
 
 
 pub struct ScreenState {
-    head: Option<Vec<String>>,
-    body: Option<Vec<String>>,
-    tail: Option<Vec<String>>,
-    longest_line_length: usize
+    head: Vec<String>,
+    body: Vec<String>,
+    tail: Vec<String>,
+    max_len: usize
 }
 
 impl ScreenState {
     pub fn new() -> Self {
-        Self { head: None, body: None, tail: None, longest_line_length: 0 }
+        Self { head: Vec::new(), body: Vec::new(), tail: Vec::new(), max_len: 0 }
     }
     
     pub fn push(&mut self, part: ScreenPart, line: String) {
+
+        // keep track of longest line length in print stack
         let line_length = line.len();
-        if line_length > self.longest_line_length {
-            self.longest_line_length = line_length;
+        if line_length > self.max_len {
+            self.max_len = line_length;
         }
 
         match part {
-            Head => { 
-                if self.head.is_none() {
-                    self.head = Some(Vec::new())
-                }
-                self.head.as_mut()
-                         .expect("Screenstate::push - Head was not created!")
-                         .push(line);
-            }
-
-            Body => {
-                if self.body.is_none() {
-                    self.body = Some(Vec::new())
-                }
-                self.body.as_mut()
-                         .expect("Screenstate::push - Body was not created!")
-                         .push(line);
-            }
-
-            Tail => {
-                if self.tail.is_none() {
-                    self.tail = Some(Vec::new())
-                }
-                self.tail.as_mut()
-                         .expect("Screenstate::push - Tail was not created!")
-                         .push(line);
-            }
+            Head => { self.head.push(line) }
+            Body => { self.body.push(line) }
+            Tail => { self.tail.push(line) }
         }
+    }
+
+    pub fn append(&mut self, part: ScreenPart, lines: &mut Vec<String>) {
+        
+        match part {
+            Head => { self.head.append(lines) }
+            Body => { self.body.append(lines) }
+            Tail => { self.tail.append(lines) }
+        }
+
     }
 
     pub fn clear(&mut self, part: ScreenPart) {
 
         match part {
-            Head => { self.head = None }
-            Body => { self.body = None }
-            Tail => { self.tail = None }
+            Head => { self.head = Vec::new() }
+            Body => { self.body = Vec::new() }
+            Tail => { self.tail = Vec::new() }
         }
 
     }
 
     pub fn print_screen(&self) {
 
-        //clear screen
+        // clear screen
         print!("{ESC}c");
 
-        if let Some(head) = &self.head {
-            for line in head {
-                print!("{}", format!("{}{}\n", " ".repeat((self.longest_line_length - line.len()) / 2), line));
-            }
-
-            println!("\n{}\n", "=".repeat(self.longest_line_length));
+        // print head
+        for line in self.head.iter() {
+            print!("{}", format!("{}{}\n", " ".repeat((self.max_len - line.len()) / 2), line));
+        }
+        if !self.head.is_empty() {
+            println!("\n{}\n", "=".repeat(self.max_len));
         }
 
-        if let Some(body) = & self.body {
-            for line in body {
-                print!("{}", line);
-            }
+        // print body
+        for line in self.body.iter() {
+            print!("{}", line);
         }
 
-        if let Some(tail) = &self.tail {
-            println!("\n{}\n", "=".repeat(self.longest_line_length));
-
-            for line in tail {
-                print!("{}", format!("{}{}", " ".repeat((self.longest_line_length - line.len()) / 2), line));
-            }
+        //print tail
+        if !self.tail.is_empty() {
+            println!("\n{}\n", "=".repeat(self.max_len));
+        }
+        for line in self.tail.iter() {
+            print!("{}", format!("{}{}", " ".repeat((self.max_len - line.len()) / 2), line));
         }
 
     }
